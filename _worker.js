85603f179536676a744ceafc33d252bfa9040b49
Ursin Saluz <ursin.saluz@laax.com>
2025-01-15 11:41:58 +0100

    add worker build instructions

---
+++ _worker.js
@@ -0,0 +1,12 @@
+addEventListener('fetch', event => {
+    event.respondWith(handleRequest(event.request))
+  })
+
+  async function handleRequest(request) {
+
+      return new Response(null, {
+        headers: {
+          'content-type': 'text/html;charset=UTF-8',
+        },
+      })
+  }
